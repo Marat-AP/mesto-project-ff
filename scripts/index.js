0d4cd4d81c9655115d77e06db92bad26a3bbf4bb
Марат <m.aptikaev@yandex.ru>
2025-01-16 23:08:09 +0300

    V.1 [5]

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,7 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
 
 function createCard(cardData, cardDelete) {
