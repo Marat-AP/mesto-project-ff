67dbe44d5c250f395d33348fab55e91ecf700b11
Марат <m.aptikaev@yandex.ru>
2025-01-16 23:04:27 +0300

    V. 1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(cardData, cardDelete) {
+    const cardTemplate = document.getElementById('card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardName = cardElement.querySelector('.card__title');
+    cardName.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        cardDelete(cardElement);
+    });
+
+    return cardElement;
+}
+
+function cardRemoval (cardElement) {
+    cardElement.remove();
+}
+
+const cardList = document.querySelector('.places__list');
+
+initialCards.forEach((item) => {
+    const cardElement = createCard(item, cardRemoval);
+    cardList.append(cardElement);
+})
